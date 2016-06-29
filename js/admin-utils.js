92f96fce52c7b427986a2fb51c0332e14870a21a
yoku2010 <yoku2010@gmail.com>
2016-06-29 21:15:52 +0530

    View library | add Library

--- js/admin-utils.js
+++ js/admin-utils.js
@@ -1,3 +1,12 @@
 $(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
+    $('[data-toggle="tooltip"]').tooltip();
+    $('ul.tab-menu>li>a').click(function(e){
+        e.preventDefault();
+        var $this = $(this), $li = $this.parent(), $ul = $li.parent(),
+            $tabPanel = $('div.tab-panel');
+        $ul.find('>li').removeClass('active');
+        $li.addClass('active');
+        $tabPanel.hide();
+        $($this.attr('href')).show();
+    }).last().click();
 });
--- js/library.js
+++ js/library.js
@@ -1,8 +1,11 @@
 $(function(){
-    var $form = $('#id_library_form');
+    var $form = $('#id_library_form'), $view = $('ul.tab-menu>li>a').last();
     $form.find('input.polymer-field, textarea.polymer-field, select.polymer-field').polymerForm({
         bar_height: '1px',
         base_color: '#CCC',
         active_color: '#F6D641'
+    });
+    $('#cancel_btn').click(function(){
+        $view.click();
     });
 });
